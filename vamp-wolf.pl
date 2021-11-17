a6c8d977a5a377b24591fbcd606ea977f4511e1f
Cesar Salcedo <cesar.salcedo@utec.edu.pe>
2021-11-17 12:51:40 -0700

    Added main predicates for computation of general purpose puzzle solver

--- vamp-wolf.pl
+++ vamp-wolf.pl
@@ -1,10 +1,4 @@
-same([], []).
-same(StateA, StateB) :-
-    StateA == StateB.
-
-is_terminal([[], [v, v, v, w, w, w], e]).
-is_terminal([[], [v, v, v, w, w, w], w]).
-
+%%% General purpose rules
 arange(0, []).
 arange(N, L) :-
     N > 0,
@@ -12,7 +6,31 @@ arange(N, L) :-
     arange(N1, L1),
     append(L1, [N1], L).
 
-% step(State, Action, NextState) :-
+
+%%% Environment rules
+action_space(vamp_wolf, State, Actions) :-
+    [East, West, BoatSide] = State,
+    (
+        (BoatSide == e, length(East, N));
+        (BoatSide == w, length(West, N))
+    ),
+    arange(N, Actions).
+
+same(vamp_wolf, StateA, StateB) :-
+    StateA == StateB.
+
+% TODO: Consider different order of elements in list
+is_terminal(vamp_wolf, [[], [v, v, v, w, w, w], BoatSide]) :-
+    BoatSide == e;
+    BoatSide == w.
+
+get_start(vamp_wolf, Start) :-
+    Start = [[v, v, v, w, w, w], [], e].
+
+observe(vamp_wolf, State, Action, NextState, Done) :-
+    true.
+
+% step(vamp_wolf, State, Action, NextState) :-
 %     [East, West, BoatSide] = State,
 %     (
 %         (BoatSide == e, Set is East);
@@ -21,17 +39,44 @@ arange(N, L) :-
 %     length(Set, N),
 %     Action >= 0, Action < N,
 
-action_space(State, Actions) :-
-    [East, West, BoatSide] = State,
+
+%%% Search method rules
+empty(BFS, L) :-
+    true.
+
+visit_next(Heuristic, L, LNext, State) :-
+    % TODO: Extract element from BFS' queue
+    true.
+
+% Main search method
+search(Puzzle, BFS, Path).
+search(Puzzle, Heuristic, Path) :-
+    get_start(Puzzle, State),
+    PriorityQueue = [State],
+    search_recur(Puzzle, Heuristic, PQ, Path).
+
+% Auxiliary recursive search method
+search_recur(Puzzle, Heuristic, [], []).
+search_recur(Puzzle, Heuristic, PQ, Path) :-
+    length(PQ, N),
+    N > 0,
+    [State|NextPQ] = PQ,
     (
-        (BoatSide == e, length(East, N));
-        (BoatSide == w, length(West, N))
-    ),
-    arange(N, Actions).
-
-search(Puzzle, Strategy, Path) :-
-    call(Puzzle, Start, action_space),
-    call(Strategy, ),
-    State is Start.
-
-% Start is [[v, v, v, w, w, w], [], e].
+        (
+            is_terminal(Puzzle, State),
+            Path is [State]
+        );
+        (
+            action_space(Puzzle, ActionSpace),
+            search_for_loop(Puzzle, Heuristic, State, ActionSpace, PQ, NextPQ),
+            search_recur(Puzzle, Heuristic, NextPQ, PrevPath),
+            append(PrevPath, State, Path)
+        )
+    ).
+    
+search_for_loop(Puzzle, Heuristic, State, [], PQ, []).
+search_for_loop(Puzzle, Heuristic, State, [Action, RestActionSpace], PQ, NewPQ) :-
+    observe(Puzzle, State, Action, NextState, _),
+    % TODO: Replace append by Heuristic
+    append(PQ, NextState, PQ1),
+    search_for_loop(Puzzle, Heuristic, State, RestActionSpace, PQ1, NewPQ).
