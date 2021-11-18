0ca397e113fd832aec2617a56ca25b1409070050
Cesar Salcedo <cesar.salcedo@utec.edu.pe>
2021-11-17 18:20:55 -0700

    Updated state representation

--- vamp-wolf.pl
+++ vamp-wolf.pl
@@ -8,24 +8,38 @@ arange(N, L) :-
 
 
 %%% Environment rules
-action_space(vamp_wolf, State, Actions) :-
-    [East, West, BoatSide] = State,
+action_space_add(vamp_wolf, L0, W, V, WDiff, VDiff, Action, L1) :-
+    WNew is W - WDiff,
+    VNew is V - VDiff,
     (
-        (BoatSide == e, length(East, N));
-        (BoatSide == w, length(West, N))
-    ),
-    arange(N, Actions).
+        WNew >= 0, VNew >= 0,
+        append(L0, [Action], L1)
+    );
+    (
+        L1 = L0
+    ).
+
+action_space(vamp_wolf, State, Actions) :-
+    [WW, VW, WE, VE, B] = State,
+    A0 = [],
+    ((B == west, W is WW, V is VW);
+    (B == east, W is WE, V is VE)),
+    action_space_add(vamp_wolf, A0, W, V, 1, 0, 0, A1),
+    action_space_add(vamp_wolf, A1, W, V, 0, 1, 1, A2),
+    action_space_add(vamp_wolf, A2, W, V, 2, 0, 2, A3),
+    action_space_add(vamp_wolf, A3, W, V, 0, 2, 3, A4),
+    action_space_add(vamp_wolf, A4, W, V, 1, 1, 4, Actions).
 
 same(vamp_wolf, StateA, StateB) :-
     StateA == StateB.
 
 % TODO: Consider different order of elements in list
-is_terminal(vamp_wolf, [[], [v, v, v, w, w, w], BoatSide]) :-
-    BoatSide == e;
-    BoatSide == w.
+is_terminal(vamp_wolf, State) :-
+    [WW, VW, _, _, _] = State,
+    WW == 0, VW == 0.
 
 get_start(vamp_wolf, Start) :-
-    Start = [[v, v, v, w, w, w], [], e].
+    Start = [3, 3, 0, 0, west].
 
 observe(vamp_wolf, State, Action, NextState, Done) :-
     true.
@@ -41,12 +55,9 @@ observe(vamp_wolf, State, Action, NextState, Done) :-
 
 
 %%% Search method rules
-empty(BFS, L) :-
-    true.
-
-visit_next(Heuristic, L, LNext, State) :-
-    % TODO: Extract element from BFS' queue
-    true.
+insert(Heuristic, PQ, State, PQNext) :-
+    % TODO: insert element to queue according to heuristic
+    append(PQ, [State], PQNext).
 
 % Main search method
 search(Puzzle, BFS, Path).
@@ -60,7 +71,7 @@ search_recur(Puzzle, Heuristic, [], []).
 search_recur(Puzzle, Heuristic, PQ, Path) :-
     length(PQ, N),
     N > 0,
-    [State|NextPQ] = PQ,
+    [State | NextPQ] = PQ,
     (
         (
             is_terminal(Puzzle, State),
@@ -70,13 +81,13 @@ search_recur(Puzzle, Heuristic, PQ, Path) :-
             action_space(Puzzle, ActionSpace),
             search_for_loop(Puzzle, Heuristic, State, ActionSpace, PQ, NextPQ),
             search_recur(Puzzle, Heuristic, NextPQ, PrevPath),
-            append(PrevPath, State, Path)
+            append(PrevPath, [State], Path)
         )
     ).
     
 search_for_loop(Puzzle, Heuristic, State, [], PQ, []).
-search_for_loop(Puzzle, Heuristic, State, [Action, RestActionSpace], PQ, NewPQ) :-
+search_for_loop(Puzzle, Heuristic, State, [Action | RestActionSpace], PQ, NewPQ) :-
     observe(Puzzle, State, Action, NextState, _),
     % TODO: Replace append by Heuristic
-    append(PQ, NextState, PQ1),
+    insert(Heuristic, PQ, NextState, PQ1),
     search_for_loop(Puzzle, Heuristic, State, RestActionSpace, PQ1, NewPQ).
