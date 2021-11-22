73d1bac2b3c41c8699fd5edac116dbef9e367c6b
Cesar Salcedo <cesar.salcedo@utec.edu.pe>
2021-11-21 19:34:27 -0700

    Updated main.pl

--- main.pl
+++ main.pl
@@ -1,1 +1,18 @@
-solve(Puzzle, Strategy).
+:- 
+    load_files(['vamp-wolf']).
+
+print_path([]).
+print_path([State | RestPath]) :-
+    write(State), nl,
+    print_path(RestPath).
+
+run(Puzzle, best_fs, Heuristic) :-
+    write("NOT IMPLEMENTED"), nl.
+    % init_heuristic(best_fs, H, Heuristic),
+    % search(Puzzle, H, Path),
+    % print_path(Path).
+
+run(Puzzle, Method) :-
+    init_heuristic(Method, H),
+    search(Puzzle, H, Path),
+    print_path(Path).
